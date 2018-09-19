bdbc9532347b858cb527403a857a16658f95f48e
SanggiHong <sanggi.hong11@gmail.com>
2018-09-19 21:15:33 +0900

    시간초과2 baekjoon10227

--- baekjoon10227/algo.py
+++ baekjoon10227/algo.py
@@ -1,9 +1,27 @@
+def find_rank(rank, arr):
+    if len(arr) == 1:
+        return arr[0]
+
+    bigger = list()
+    smaller = list()
+    pivot = arr.pop()
+    for i in arr:
+        smaller.append(i) if i < pivot else bigger.append(i)
+
+    if len(smaller) == rank:
+        return pivot
+    elif len(smaller) > rank:
+        return find_rank(rank, smaller)
+    else:
+        return find_rank(rank - len(smaller) - 1, bigger)
+
+
 a = [int(i) for i in input().split()]
 R = a[0]
 C = a[1]
 H = a[2]
 W = a[3]
-mat = [[0]*C for i in range(R)]
+mat = [[0] * C for i in range(R)]
 
 for r in range(R):
     a = input().split()
@@ -11,12 +29,12 @@ for r in range(R):
         mat[r][c] = int(a[c])
 
 ans = list()
-for r in range(H-1, R):
-    for c in range(W-1, C):
-        arr = list()
-        for item in mat[r-H+1:r+1]:
-            arr.extend(item[c-W+1:c+1])
-        arr.sort()
-        ans.append(arr[4])
+arr = list()
+for r in range(H - 1, R):
+    for c in range(W - 1, C):
+        arr.clear()
+        for item in mat[r - H + 1:r + 1]:
+            arr.extend(item[c - W + 1:c + 1])
+        ans.append(find_rank(H * W // 2, arr))
 
 print(min(ans))
